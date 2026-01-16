db1e04376c186d890e5600cbe0809c1aa324bd73
Saheed Opeyemi Ibrahim <horwolhabi@gmail.com>
2026-01-16 18:04:49 +0100

    Initial commit: Hello World Project

---
+++ intro.js
@@ -0,0 +1,6 @@
+const name = "Saheed Opeyemi Ibrahim";
+const age = 26;
+const favouriteLanguage = "Yoruba";
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favourite Language:", favouriteLanguage);
